5534cbfc954fcb18adf9741cea7066b27125e2b2
赵聃 <zhaodan@corp.51xiaodou.com>
2016-07-18 22:43:50 +0800

    修改配置/填充Interface字段

--- xd-autotest-framework/api/controllers/InterfaceController.js
+++ xd-autotest-framework/api/controllers/InterfaceController.js
@@ -6,8 +6,28 @@
  */
 
 module.exports = {
-	hello: function (req, res) {
-    res.send("HelloWorld!");
+  hello: function (req, res) {
+    var iter = {name: "test", version: "1.0", description: "it's a desc", url: "192.168.88.89:8042", inputFile: "123", outputFile: "456"};
+    Interface.create(iter).exec(function createCB(err, created) {
+      if (err) {
+        // 如果有误，返回错误
+        res.view('passport/register', {err: err});
+      } else {
+        // 否则，将新创建的用户登录
+        res.send("Ok");
+      }
+    });
+  },
+  hello2: function (req, res) {
+    Interface.findOne({name: "test"}).exec(function (err, articles) {
+      if (!err) {
+        // 刷新下一页
+        return res.send(articles);
+      }
+      else {
+        console.log(err);
+      }
+    });
   }
 };
 
--- xd-autotest-framework/api/models/Interface.js
+++ xd-autotest-framework/api/models/Interface.js
@@ -4,11 +4,74 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var uuid = require('node-uuid');
 
 module.exports = {
-
+  tableName: 'xd_autotest_interface',
   attributes: {
+    id: {
+      type: 'string',
+      required: true,
+      primaryKey: true,
+      defaultsTo: function () {
+        return uuid.v4();
+      }
+    },
+    // base info
+    name: {
+      type: 'string',
+      required: true,
+      minLength: 1,
+      maxLength: 60
+    },
+    version: {
+      type: 'string',
+      required: true,
+      minLength: 1,
+      maxLength: 20
+    },
+    description: {
+      type: 'string',
+      required: true,
+      minLength: 1,
+      maxLength: 300
+    },
 
+    // req info
+    url: {
+      type: 'string',
+      required: true,
+      minLength: 1,
+      maxLength: 300
+    },
+    protocol: {
+      type: 'string',
+      enum: ['http', 'https'],
+      defaultsTo: 'http',
+      required: true
+    },
+    method: {
+      type: 'string',
+      enum: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'COPY', 'HEAD', 'OPTIONS', 'LINK', 'UNLINK', 'PURGE', 'LOCK', 'UNLOCK', 'PROPFIND', 'VIEW'],
+      defaultsTo: 'GET',
+      required: true
+    },
+    headed: {
+      type: 'array'
+    },
+    bodyType: {
+      type: 'string',
+      minLength: 1,
+      maxLength: 30
+    },
+    inputFile: {
+      type: 'longtext',
+      required: true
+    },
+    outputFile: {
+      type: 'longtext',
+      required: true
+    }
   }
 };
 
