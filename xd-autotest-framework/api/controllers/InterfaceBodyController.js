8256f22349b27c23254ee8a3a9f0d3e435310d62
赵聃 <zhaodan@corp.51xiaodou.com>
2016-07-19 16:46:59 +0800

    完善api

---
+++ xd-autotest-framework/api/controllers/InterfaceBodyController.js
@@ -0,0 +1,11 @@
+/**
+ * InterfaceBodyController
+ *
+ * @description :: Server-side logic for managing Interfacebodies
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
