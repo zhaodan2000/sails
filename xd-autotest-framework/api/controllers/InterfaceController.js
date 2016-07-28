57e1c6b9ccc84c58a413c70eb565e2df93f06ae9
liyuehua2016 <2105567833@qqq.com>
2016-07-28 15:06:14 +0800

    test abc....

--- xd-autotest-framework/api/controllers/InterfaceController.js
+++ xd-autotest-framework/api/controllers/InterfaceController.js
@@ -1,6 +1,6 @@
 /**
  * InterfaceController
- *
+ * ***
  * @description :: Server-side logic for managing Interfaces
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
