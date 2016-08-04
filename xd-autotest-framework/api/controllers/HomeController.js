17d795019af3e83917bdb445b33ddee1f74f17ae
赵聃 <zhaodan@corp.51xiaodou.com>
2016-08-04 15:43:04 +0800

    添加模板框架/删除无效资源

---
+++ xd-autotest-framework/api/controllers/HomeController.js
@@ -0,0 +1,13 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing Homes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index : function(req, res) {
+    res.view('index');
+  }
+};
+
