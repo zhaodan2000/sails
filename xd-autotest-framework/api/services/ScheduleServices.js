edd843e3fccbe0c5ca2203e8e66193595af89bdd
2696588656@qq.com <2696588656@qq.com>
2016-10-09 16:57:41 +0800

    GAS

--- xd-autotest-framework/api/services/ScheduleServices.js
+++ xd-autotest-framework/api/services/ScheduleServices.js
@@ -1,52 +1,36 @@
 /**
- * Created by xiaodou_chenxiaoxiang on 16/8/24.
+ * Created by zhouhuanon 16/10/9.
  */
 module.exports = {
     /**
      * 根据任务的调度策略类型Schedule_ID进行调度
      * @param scheduleID
      */
-    execTaskWithScheduleID: function(scheduleID)
+    execTaskWithScheduleID: function(sc_id)
   {
 
-    console.log(scheduleID + '-------------');
+    console.log(sc_id + '-------------');
     var schedule = require("node-schedule");
-    /**
-     * 根据id获取调度要使用的时间
-     */
-    var scheduleObj = getScheduleTimeWithID(scheduleID);
-    var timeArr = scheduleObj.schedule_time.toString().split(":");
-
-    //配置调度任务
-    var rule = new schedule.RecurrenceRule();
-
-    rule.dayOfWeek = scheduleObj.day_of_week;
-
-    rule.hour = timeArr[0];
-
-    rule.minute = timeArr[1];
-
-    var j = schedule.scheduleJob(rule, function () {
-      //执行任务
-      console.log("执行任务3");
-      this.findTaskWithSchedule(scheduleID);
-    });
+    var scArr = getSc();
+    for (var i=0;i<scArr.length;i++){
+       var sc=scArr[i];
+      var j = schedule.scheduleJob(sc.sc_time, function () {
+        console.log("执行任务");
+      });
+    }
   },
+
 
   /**
    * 查找schedule
    * @param scheduleID
    */
-   getScheduleTimeWithID: function(scheduleID) {
-    mongoService.Find('ScheduleStrategy', {uniqId: scheduleID}, function (record) {
-      if (record) {
-        var scheduleObj = record[0];
-        return scheduleObj;
-      }
-    })
+  getSc: function() {
+    mongoService.Find('ScheduleTask',null,function (records) {
+      console.log(records);
+      return records;
+    });
   },
 
-  findTaskWithSchedule: function(scheduleID){
-      
-  }
+
 }
