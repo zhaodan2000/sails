e2d222c97fd857b0875ed8710c60cb54b43e76ec
2696588656@qq.com <2696588656@qq.com>
2016-10-10 16:38:39 +0800

    关于日志

---
+++ xd-autotest-framework/api/models/ScheduleLog.js
@@ -0,0 +1,32 @@
+/**
+ * ScheduleLog.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  connection: 'someMongodbServer',
+  tableName: 'xd_autotest_ScheduleLog',
+  autoPK:true,
+  attributes: {
+    id:{
+      type:'string',
+      required:false,
+      primaryKey:true
+    },
+    log_id:{
+      type:'string',
+      required:false,
+      unique:true
+    },
+    sc_id:{
+      type:'string',
+      required:false,
+    },
+    log_desc:{
+      type:'string',
+      required:false,
+    },
+  }
+};
