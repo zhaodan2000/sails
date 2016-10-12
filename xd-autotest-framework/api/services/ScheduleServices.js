2afa9f48952c9e0163f9973a704f35a858d22660
2696588656@qq.com <2696588656@qq.com>
2016-10-12 18:09:49 +0800

    no message

--- xd-autotest-framework/api/services/ScheduleServices.js
+++ xd-autotest-framework/api/services/ScheduleServices.js
@@ -6,6 +6,7 @@ var service = require("../services/CaseServices");
 var collectionHelper = require('../newman/NewManModel');
 var eventproxy = require('../utils/eventproxyhelper');
 var mongoService = require("../services/mongoService");
+var mailService = require("MailService");
 var nodemailer = require("nodemailer");
 var smtpTransport = require('nodemailer-smtp-transport');
 var request = require('request');
@@ -101,7 +102,8 @@ module.exports = {
         };
         mongoService.Insert("ScheduleLog", log, function (records) {
           if (records) {
-            request.post('http://localhost:1337/log/sendMail').form({"log_id":log_id})
+           /* request.post('http://localhost:1337/log/sendMail').form({"log_id":log_id})*/
+            mailService.sendMail();
             return log_id;
           } else {
             //fail
@@ -136,14 +138,9 @@ module.exports = {
       }
     });
   },
-
-  sendMail:function() {
-    _sendMail();
-  },
 }
 
 exports.execute = _execute;
-exports.execute=_sendMail;
 function _execute(itemArr,sc_id) {
   var ep = eventproxy.create();
   var collection = collectionHelper.newCollection();
@@ -176,36 +173,4 @@ function _execute(itemArr,sc_id) {
       ep.emit(1);
     });
   }
-}
-
-
-function _sendMail(){
-  // 开启一个 SMTP 连接池
-  var transport = nodemailer.createTransport(smtpTransport({
-    host: "smtp.ym.163.com", // 主机
-    secure: true, // 使用 SSL
-    port: 994, // SMTP 端口
-    auth: {
-      user: "zhouhuan@corp.51xiaodou.com", // 账号
-      pass: "zhouhuan123" // 密码
-    }
-  }));
-// 设置邮件内容
-
-  var mailOptions = {
-    from: "zhouhuan@corp.51xiaodou.com", // 发件地址
-    to: "liyuehua@corp.51xiaodou.com", // 收件列表
-    subject: "周欢测试", // 标题
-    html: "<b>thanks a for visiting!</b>"
-  }
-// 发送邮件
-
-  transport.sendMail(mailOptions, function(error, response) {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(response);
-    }
-    transport.close(); // 如果没用，关闭连接池
-  });
 }
