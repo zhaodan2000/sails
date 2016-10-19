ad4db99883c1c67db6ea2d8a714de00dd241273b
赵聃 <zhaodan@corp.51xiaodou.com>
2016-10-19 15:34:17 +0800

    添加deploy腳本

---
+++ xd-autotest-framework/script/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##path##
+basedir=/home/editor/sails
+baseop=xd-autotest-framework
+
+function init_env
+{
+	mkdir -p $basedir/$baseop
+}
+
+function deploy
+{
+	cd $basedir/$baseop
+	tar zxvf $basedir/$baseop/*.tar
+  #cp -rf $basedir/$baseop/conf/* /home/work/xiaodou/$baseop/WEB-INF/classes/conf/custom/env
+	#rm -rf /home/work/xiaodou/$baseop/*.war
+#	sh /home/work/xiaodou/$tomcat/bin/stop_tomcat.sh
+#	sh /home/work/xiaodou/$tomcat/bin/start_tomcat.sh
+  npm install
+  /bin/bash ../start.sh
+}
+
+init_env
+deploy
